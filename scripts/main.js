5fd73dd38592724df2f1984caf31f92dae54110f
bbouslog <36169367+bbouslog@users.noreply.github.com>
2024-03-13 16:44:57 -0500

    Finish Bill Add

--- scripts/main.js
+++ scripts/main.js
@@ -6,14 +6,14 @@ function add_checking_bill() {
     var tot_bills = 0;
     var output_html = "";
     for (let i = 0; i < table_row_str.length; i++) {
-        if (table_row_str[i].includes("c-bil-new")) {
-            tot_bills += 1;
-        }
         if (table_row_str[i].includes("c-bil-add")) {
             output_html += "\n<tr>\n";
             output_html += "\t<td><h4>Bill Amount</h4></td>\n";
-            output_html += "\t<td><input type=\"text\" id=\"c-bil-" + tot_bills.toString() + "\"></td>\n";
+            output_html += "\t<td><input type=\"text\" class=\"c-input\" id=\"c-bil-" + tot_bills.toString() + "\"></td>\n";
             output_html += "</tr>";
+        }
+        else if (table_row_str[i].includes("c-bil")) {
+            tot_bills += 1;
         }
         
         output_html += table_row_str[i]
@@ -26,5 +26,4 @@ function add_checking_bill() {
     document.getElementById("c-table").innerHTML = output_html;
     document.getElementById("c-bil-add").onclick = add_checking_bill;
 }
-
 document.getElementById("c-bil-add").onclick = add_checking_bill;
