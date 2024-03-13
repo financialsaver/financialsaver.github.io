98fd03c619067395005bf3de2cf21dad508abb84
bbouslog <36169367+bbouslog@users.noreply.github.com>
2024-03-13 15:12:35 -0500

    Adding Checking Section

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+
+function add_checking_bill() {
+    var table_str = document.getElementById("c-table").value;
+
+    console.log(table_str);
+}
+
+document.getElementById("c-bil-add").onclick = add_checking_bill;
