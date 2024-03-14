1b0494e24d9f00922d2f366a75048912855bf05d
bbouslog <36169367+bbouslog@users.noreply.github.com>
2024-03-14 15:46:04 -0500

    Add Loans

--- scripts/main.js
+++ scripts/main.js
@@ -86,11 +86,11 @@ function add_savings_bucket() {
         if (table_row_str[i].includes("s-bkt-add")) {
             output_html += "\n<tr>\n";
             output_html += "\t<td><h4>Bucket #" + (tot_bkts+1).toString() + "</h4></td>\n";
-            output_html += "\t<td><input type=\"text\" class=\"s-input\" id=\"s-bkt-bal-" + tot_bills.toString() + "\"></td>\n";
-            output_html += "\t<td><input type=\"text\" class=\"s-input\" id=\"s-bkt-rat-" + tot_bills.toString() + "\"></td>\n";
-            output_html += "\t<td><input type=\"text\" class=\"s-input\" id=\"s-bkt-lim-" + tot_bills.toString() + "\"></td>\n";
-            output_html += "\t<td><p id=\"s-bkt-rec-" + tot_bills.toString() + "\"></td>\n";
-            output_html += "\t<td><input type=\"text\" class=\"s-input\" id=\"s-bkt-act-" + tot_bills.toString() + "\"></td>\n";
+            output_html += "\t<td><input type=\"text\" class=\"s-input\" id=\"s-bkt-bal-" + tot_bkts.toString() + "\"></td>\n";
+            output_html += "\t<td><input type=\"text\" class=\"s-input\" id=\"s-bkt-rat-" + tot_bkts.toString() + "\"></td>\n";
+            output_html += "\t<td><input type=\"text\" class=\"s-input\" id=\"s-bkt-lim-" + tot_bkts.toString() + "\"></td>\n";
+            output_html += "\t<td><p id=\"s-bkt-rec-" + tot_bkts.toString() + "\">0</p></td>\n";
+            output_html += "\t<td><input type=\"text\" class=\"s-input\" id=\"s-bkt-act-" + tot_bkts.toString() + "\"></td>\n";
             output_html += "</tr>";
         }
         else if (table_row_str[i].includes("s-bkt")) {
@@ -121,7 +121,63 @@ function add_savings_bucket() {
     }
     
     /* Restoring Event Listeners */
-    document.getElementById("s-bil-add").onclick = add_savings_bucket;
+    document.getElementById("s-bkt-add").onclick = add_savings_bucket;
+    
+    /*
+    var s_input_elems = document.getElementsByClassName("s-input");
+    for (let i = 0; i < s_input_elems.length; i++) {
+        s_input_elems[i].addEventListener("change", update_savings_transfer);
+    }
+    */
+}
+
+function add_loan() {
+    var table_str = document.getElementById("s-table").innerHTML;
+    const table_row_str = table_str.split("</tr>");
+
+    /* Creating the Output HTML */
+    var tot_loans = 0;
+    var output_html = "";
+    for (let i = 0; i < table_row_str.length; i++) {
+        if (table_row_str[i].includes("s-lon-add")) {
+            output_html += "\n<tr>\n";
+            output_html += "\t<td><h4>Loan #" + (tot_loans+1).toString() + "</h4></td>\n";
+            output_html += "\t<td><input type=\"text\" class=\"s-input\" id=\"s-lon-bal-" + tot_loans.toString() + "\"></td>\n";
+            output_html += "\t<td><input type=\"text\" class=\"s-input\" id=\"s-lon-rat-" + tot_loans.toString() + "\"></td>\n";
+            output_html += "\t<td><input type=\"text\" class=\"s-input\" id=\"s-lon-lim-" + tot_loans.toString() + "\"></td>\n";
+            output_html += "\t<td><p id=\"s-lon-rec-" + tot_loans.toString() + "\">0</p></td>\n";
+            output_html += "\t<td><input type=\"text\" class=\"s-input\" id=\"s-lon-act-" + tot_loans.toString() + "\"></td>\n";
+            output_html += "</tr>";
+        }
+        else if (table_row_str[i].includes("s-bkt")) {
+            tot_loans += 1;
+        }
+        
+        output_html += table_row_str[i];
+        if (i < table_row_str.length - 1) {
+            output_html += "</tr>";
+        }
+    }
+
+    /* Saving Input Values */
+    var s_input_elems = document.getElementsByClassName("s-input");
+    var s_input_ids = [];
+    var s_input_vals = [];
+    for (let i = 0; i < s_input_elems.length; i++) {
+        s_input_ids.push(s_input_elems[i].id);
+        s_input_vals.push(s_input_elems[i].value);
+    }
+    
+    /* Reseting to New HTML */
+    document.getElementById("s-table").innerHTML = output_html;
+
+    /* Restoring Original Input Values */
+    for (let i = 0; i < s_input_ids.length; i++) {
+        document.getElementById(s_input_ids[i]).value = s_input_vals[i];
+    }
+    
+    /* Restoring Event Listeners */
+    document.getElementById("s-lon-add").onclick = add_savings_bucket;
     
     /*
     var s_input_elems = document.getElementsByClassName("s-input");
@@ -141,3 +197,5 @@ for (let i = 0; i < c_input_elems.length; i++) {
 }
 
 document.getElementById("s-bkt-add").onclick = add_savings_bucket;
+
+document.getElementById("s-lon-add").onclick = add_loan;
